be148a0ad7dcb71d8e20aa6371e0a53a6033ffe0
AngerOverApathy <violasian@gmail.com>
2022-12-17 13:57:06 -0800

    stories formatted into readable list

--- pages/stories.js
+++ pages/stories.js
@@ -1,12 +1,13 @@
 import view from '../utils/view.js';
+import Story from '../components/story.js';
 
 export default async function Stories(path) {   //display path on page
   const stories = await getStories(path);
   const hasStories = stories.length > 0 //check if there are stories 
 
   view.innerHTML = `<div>
-  ${hasStories ? stories.map(story => JSON.stringify(story)) : 'No stories'} //if there are stories display stories else 'no stories'
-  </div>`;  
+  ${hasStories ? stories.map((story, i) => Story({ ...story, index: i + 1 })).join('') : 'No stories'} //if there are stories display stories else 'no stories'
+  </div>`;                   //add i to existing story data by creating in line object, spread in properties with '...' and create index property on story (i + 1)
 }
 
 async function getStories(path) {
