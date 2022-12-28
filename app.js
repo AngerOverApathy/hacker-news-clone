322a39fb642fabfdd8ddbd96b94ccaba08af3379
AngerOverApathy <violasian@gmail.com>
2022-12-28 12:10:20 -0800

    added style to selected icons for user visibility

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 import RouterHandler from "./router.js";
 
+//onhashchange detects hash in url and its changes
+window.onhashchange = () => {
+    setActiveLink();
+}
+
+function setActiveLink() {
+    const links = document.querySelectorAll('.header-link');
+    links.forEach(link => {
+       const linkPath = link.getAttribute('href');
+       const currentPath = window.location.hash;
+       if (currentPath === linkPath) {
+         link.classList.add('active');  //add active class for visbility if current and link paths match
+       } else {
+         link.classList.remove('active');         
+       }
+    });
+ }
+
 class App {
     constructor() {
         new RouterHandler(); //instantiate with 'new'
