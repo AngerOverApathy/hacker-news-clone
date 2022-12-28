76bceb11b9dd4262c1e3b94d250e6727d0691e6a
AngerOverApathy <violasian@gmail.com>
2022-12-28 12:04:33 -0800

    comment component created and styled

--- components/comment.js
+++ components/comment.js
@@ -1,10 +1,16 @@
-function Comment(comment) {
+export default function Comment(comment) {
+  const hasNestedComments = comment.comments.length > 0; //comment length >0 === true
+
+    //recursive function, map over comments to check for nested comments
+    //join comments together else return empty string
+    //use comment.level to apply css properties to specific level of comments
     return `
-      <div class="nested-comments-0">
+      <div class="nested-comments-${comment.level}">
         <p class="comment-header">
           ${comment.user} | ${comment.time_ago}
         </p>
         ${comment.content}
+        ${hasNestedComments ? comment.comments.map( comment => Comment(comment)).join('') : ''}  
       </div>
     `  
   }
